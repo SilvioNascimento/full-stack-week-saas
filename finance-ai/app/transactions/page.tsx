9e1b960af7c85b0c0a44b09957aeb63287b93817
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-08 20:26:54 -0300

    feat: create add transaction dialog

--- finance-ai/app/transactions/page.tsx
+++ finance-ai/app/transactions/page.tsx
@@ -1,8 +1,7 @@
 import { db } from "../_lib/prisma";
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   // Acessar as transações no meu banco de dados
@@ -12,9 +11,7 @@ const TransactionsPage = async () => {
       {/*TÍTULO E BOTÃO*/}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full font-bold">
-          Adicionar transação <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton/>
       </div>
 
       <DataTable columns={transactionColumns} data={transactions} />
