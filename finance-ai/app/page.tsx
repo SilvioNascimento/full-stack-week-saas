849dbb8d038858fdebfe68aeff748fd1525e2c39
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-07 22:05:48 -0300

    test3

--- finance-ai/app/page.tsx
+++ finance-ai/app/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const name = ""; 
+const name2 = "";
 
 const Home = async () => {
   const { userId } = await auth();
