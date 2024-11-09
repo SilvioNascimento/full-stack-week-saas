4124fbd760413b85fda28e339cff4f54173b74c9
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-08 22:39:54 -0300

    feat: ensure transaction can be updated

--- finance-ai/app/transactions/page.tsx
+++ finance-ai/app/transactions/page.tsx
@@ -11,7 +11,7 @@ const TransactionsPage = async () => {
       {/*TÍTULO E BOTÃO*/}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton/>
+        <AddTransactionButton />
       </div>
 
       <DataTable columns={transactionColumns} data={transactions} />
