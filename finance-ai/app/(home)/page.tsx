e7225eb960389c5b555c6865b6ecf7c2f119e693
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-10 16:36:46 -0300

    feat: add summary cards

---
+++ finance-ai/app/(home)/page.tsx
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
+
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  // Verifica se o mês que foi inserido no query params está com formato correto ou se não foi fornecido
+  const monthIsInvalid = !month || !isMatch(month, 'MM')
+  if(monthIsInvalid) {
+    redirect('/?month=01')
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+      </div>
+
+      <SummaryCards month={month} />
+    </>
+  );
+};
+
+export default Home;
