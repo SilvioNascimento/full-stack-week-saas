56df6c708047e99624f16b268e87627bae7dd403
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-09 22:35:20 -0300

    feat: ensure transactions and subscriptions pages are protected

--- finance-ai/app/subscription/page.tsx
+++ finance-ai/app/subscription/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 
-const SubscriptionPage = () => {
+const SubscriptionPage = async () => {
+    const {userId} = await auth();
+  // Se tentar acessar a página de assinatura sem estar logado, então será redirecionado para a página de login
+  if (!userId) {
+    redirect("/login")
+  }
     return(
         <Navbar/>
     );
--- finance-ai/app/transactions/page.tsx
+++ finance-ai/app/transactions/page.tsx
@@ -3,10 +3,21 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
+  const {userId} = await auth();
+  // Se tentar acessar a página de transação sem estar logado, então será redirecionado para a página de login
+  if (!userId) {
+    redirect("/login")
+  }
   // Acessar as transações no meu banco de dados
-  const transactions = await db.transaction.findMany({});
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    }
+  });
   return (
     <>
       <Navbar />
